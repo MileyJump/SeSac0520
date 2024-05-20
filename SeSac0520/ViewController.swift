edeccccfcc50e12760165ce9de40211b7f11fcc9
choiminkyung <choialsrud2002@gmail.com>
2024-05-20 11:43:29 +0900

    UIView tag 활용해서 코드 간단하게 만들기 / @IBAction을 여러 버튼에 연결

--- SeSac0520/ViewController.swift
+++ SeSac0520/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController {
     @IBOutlet var twoLabel: UILabel!
     @IBOutlet var threeLabel: UILabel!
     
+    var count = [0, 0, 0]
+    
+    var oneCount = 0
+    var twoCount = 0
+    var threeCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,20 +33,31 @@ class ViewController: UIViewController {
         designLabelUI(threeLabel, thisIsTextColor: .red)
       
         designButtonUI(oneButton, titleColor: .blue, title: "클릭") // (): 함수 호출 연산자
-        designButtonUI(twoButton, titleColor: .green, title: "클릭")
-        designButtonUI(threeButton, titleColor: .brown, title: "클릭")
+        designButtonUI(twoButton, titleColor: .green, title: "클릭2")
+        designButtonUI(threeButton, titleColor: .brown, title: "클릭3")
     }
     
+    // 1. 어떤 버튼을 클릭했는지 어떻게 아나요?
+    // -> currentTitle(옵셔널 및 버전 조심)
+    // -> tag
     @IBAction func oneButtonClicked(_ sender: UIButton) {
-        oneLabel.text = "4567"
+        
+        count[sender.tag] = count[sender.tag] + 1
+        
+    
+        oneLabel.text = "\(count[0])번"
+        twoLabel.text = "\(count[1])번"
+        threeLabel.text = "\(count[2])번"
     }
+    
+    
     
     // 매개변수 (Parameter)
     // 외부 매개변수(Argument Label) ex. thisIsTextColor
     // 내부 매개변수(Parameter Name) ex. colorName
     // _ : 와일드카드식별자(
     func designLabelUI(_ a : UILabel, thisIsTextColor colorName: UIColor) {
-        a.text = "0"
+        a.text = "0번"
         a.textColor = colorName
         a.font = .boldSystemFont(ofSize: 30)
         a.textAlignment = .center
